f94bf2cfe9d9b8b58171806e92c60e0fa02fd5d6
Janosz Dewberry <janosz@pop-os.localdomain>
2023-12-04 14:12:55 -0500

    initial commit

--- polyrot/__init__.py
+++ polyrot/__init__.py
@@ -1,19 +1,20 @@
 ############################################################################
-# This module is a part of the Polyrot numerical package.  
+# This module is a part of the polyrot numerical package. polyrot computes 
+# polytropic models of rapidly rotating planets and stars
 #
 # Copyright (C) 2023 Janosz Dewberry
 #
-# This program is free software: you can redistribute it and/or modify it 
+# polyrot is free software: you can redistribute it and/or modify it 
 # under the terms of the GNU General Public License as published by the 
 # Free Software Foundation, version 3.
 #
-# Polyrot is distributed in the hope that it will be useful, but WITHOUT ANY 
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
-# details.
+# polyrot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along 
-# with this program. If not, see http://www.gnu.org/licenses/.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ############################################################################
 
 from .star import *
